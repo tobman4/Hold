34e734be896e72e58c1e697a67199c2d8adf3c0c
tobman4 <36448579+tobman4@users.noreply.github.com>
2026-05-16 16:33:22 +0000

    Setup basic db context with sqlite

--- src/Hold.API/Data/DB.cs
+++ src/Hold.API/Data/DB.cs
@@ -1,1 +1,14 @@
-// DbContext here
+using Microsoft.EntityFrameworkCore;
+using Hold.API.Data.Models;
+
+namespace Hold.API.Data;
+
+public class HoldDbContext : DbContext
+{
+    public HoldDbContext(DbContextOptions<HoldDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<HoldItem> HoldItems { get; set; } = null!;
+}
--- src/Hold.API/Program.cs
+++ src/Hold.API/Program.cs
@@ -1,4 +1,9 @@
+using Hold.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHoldDb(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
